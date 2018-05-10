9e31337c2216ce3d78ff670ce6b88fac1a42a2b7
josephcsoti <josephcsoti@gmail.com>
2018-05-10 12:47:44 -0700

    Added branches to print each "word"

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -1,19 +1,33 @@
+# Strings
 .data
+	str_feed: .asciiz "Feed\n"
+	str_babe: .asciiz "Babe\n"
+	str_feedbabe: .asciiz "FeedBabe\n"
 
-# Strings to be printed
-str_feed: .asciiz "Feed\n"
-str_babe: .asciiz "Babe\n"
-str_feedbabe: .asciiz "FeedBabe\n"
-
-#loop
-.text 
+#
 main:
-	li  $v0, 4
-	la  $a0, str_feed
+	j out_feed
+	j out_babe
+	j out_feedbabe
 	syscall
 	
-#Print "Feed"
-
-#Print "Babe"
-
-#Print "FeedBabe
+# Print "Feed"
+.text
+	out_feed:
+		li  $v0, 4
+		la  $a0, str_feed
+		syscall
+		
+# Print "Babe"
+.text
+	out_babe:
+		li  $v0, 4
+		la  $a0, str_babe
+		syscall
+		
+# Print "FeedBabe"
+.text
+	out_feedbabe:
+		li  $v0, 4
+		la  $a0, str_feedbabe
+		syscall
